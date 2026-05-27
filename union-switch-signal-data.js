466c46a4d8fd8c2f66592864159a52b98ca3ceee
PDFuse <davidikuta@gmail.com>
2026-05-27 13:48:24 -0500

    Add US&S data for WWII production

---
+++ union-switch-signal-data.js
@@ -0,0 +1,11 @@
+var ussData = [
+
+{
+    manufacturer: "Union Switch & Signal",
+    year: "1943",
+    serial_start: 1041405,
+    serial_end: 1096404,
+    notes: "US&S WWII production"
+}
+
+];
