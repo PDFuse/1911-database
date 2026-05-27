33a69b17280a2f6b84b354ed868c0e2836884eb8
PDFuse <davidikuta@gmail.com>
2026-05-27 13:29:12 -0500

    Refactor pistols array declaration to use var

--- data.js
+++ data.js
@@ -1,27 +1,23 @@
-const pistols = [
-
-{
-    manufacturer: "Colt",
-    year: "1945",
-    serial_start: 2244804,
-    serial_end: 2380013,
-    notes: "Late WWII Colt production"
-},
-
-{
-    manufacturer: "Ithaca",
-    year: "1945",
-    serial_start: 2075104,
-    serial_end: 2134403,
-    notes: "Late war Ithaca production"
-},
-
-{
-    manufacturer: "Singer",
-    year: "1942",
-    serial_start: "S800001",
-    serial_end: "S800500",
-    notes: "Singer contract pistols"
-}
-
+var pistols = [
+    {
+        manufacturer: "Colt",
+        year: "1945",
+        serial_start: 2244804,
+        serial_end: 2380013,
+        notes: "Late WWII Colt production"
+    },
+    {
+        manufacturer: "Ithaca",
+        year: "1945",
+        serial_start: 2075104,
+        serial_end: 2134403,
+        notes: "Late war Ithaca production"
+    },
+    {
+        manufacturer: "Singer",
+        year: "1942",
+        serial_start: "S800001",
+        serial_end: "S800500",
+        notes: "Singer contract pistols"
+    }
 ];
