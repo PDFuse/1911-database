ba14b7dbf56a155acfab6e5f1456a433fb147474
PDFuse <davidikuta@gmail.com>
2026-05-27 16:58:34 -0500

    Update data.js

--- data.js
+++ data.js
@@ -9,6 +9,7 @@ var pistols = [
     serial_end: 17250,
     notes: "Original Colt production"
 },
+
 {
     manufacturer: "Colt",
     year: "1913",
@@ -16,152 +17,71 @@ var pistols = [
     serial_end: 60400,
     notes: "Early Colt production"
 },
+
 {
     manufacturer: "Colt",
     year: "1914",
     serial_start: 60401,
-    serial_end: 72750,
+    serial_end: 72570,
     notes: "Colt production"
 },
+
 {
     manufacturer: "Colt",
-    year: "1914",
+    year: "1915",
     serial_start: 83856,
-    serial_end: 102596,
+    serial_end: 107596,
     notes: "Colt production"
 },
-{
-    manufacturer: "Colt",
-    year: "1915",
-    serial_start: 107597,
-    serial_end: 113496,
-    notes: "Colt production"
-},
-{
-    manufacturer: "Colt",
-    year: "1915",
-    serial_start: 120567,
-    serial_end: 125566,
-    notes: "Colt production"
-},
+
 {
     manufacturer: "Colt",
     year: "1916",
+    serial_start: 107597,
+    serial_end: 133186,
+    notes: "Colt production"
+},
+
+{
+    manufacturer: "Colt",
+    year: "1917",
     serial_start: 133187,
-    serial_end: 137400,
-    notes: "Colt production"
-},
-{
-    manufacturer: "Colt",
-    year: "1917",
-    serial_start: 137401,
-    serial_end: 210386,
-    notes: "Colt production"
-},
-{
-    manufacturer: "Colt",
-    year: "1917",
-    serial_start: 210387,
-    serial_end: 215386,
-    notes: "Frames only"
-},
-{
-    manufacturer: "Colt",
-    year: "1917",
-    serial_start: 215387,
     serial_end: 216986,
-    notes: "Colt production"
+    notes: "WWI Colt production"
 },
+
 {
     manufacturer: "Colt",
     year: "1918",
     serial_start: 216987,
-    serial_end: 594000,
+    serial_end: 629500,
     notes: "WWI Colt production"
 },
-{
-    manufacturer: "Colt",
-    year: "1919",
-    serial_start: 594001,
-    serial_end: 629500,
-    notes: "Post-WWI Colt production"
-},
+
 {
     manufacturer: "Colt",
     year: "1924",
     serial_start: 700001,
     serial_end: 710000,
-    notes: "Interwar Colt production"
+    notes: "M1911A1 transition production"
 },
+
 {
     manufacturer: "Colt",
-    year: "1937",
+    year: "1937-1945",
     serial_start: 710001,
-    serial_end: 712349,
-    notes: "Pre-WWII Colt production"
-},
-{
-    manufacturer: "Colt",
-    year: "1938",
-    serial_start: 712350,
-    serial_end: 713645,
-    notes: "Pre-WWII Colt production"
-},
-{
-    manufacturer: "Colt",
-    year: "1939",
-    serial_start: 713646,
-    serial_end: 717281,
-    notes: "Pre-WWII Colt production"
-},
-{
-    manufacturer: "Colt",
-    year: "1940",
-    serial_start: 717282,
-    serial_end: 721977,
-    notes: "Pre-WWII Colt production"
-},
-{
-    manufacturer: "Colt",
-    year: "1941",
-    serial_start: 721978,
-    serial_end: 756733,
-    notes: "Early WWII Colt production"
-},
-{
-    manufacturer: "Colt",
-    year: "1942",
-    serial_start: 756734,
-    serial_end: 800000,
-    notes: "WWII Colt production"
-},
-{
-    manufacturer: "Colt",
-    year: "1942",
-    serial_start: 800501,
-    serial_end: 856100,
-    notes: "WWII Colt production"
-},
-{
-    manufacturer: "Colt",
-    year: "1943",
-    serial_start: 1096405,
-    serial_end: 1208673,
-    notes: "WWII Colt production"
-},
-{
-    manufacturer: "Colt",
-    year: "1944",
-    serial_start: 1609529,
-    serial_end: 1743846,
-    notes: "WWII Colt production"
-},
-{
-    manufacturer: "Colt",
-    year: "1945",
-    serial_start: 2244804,
     serial_end: 2380013,
-    notes: "Late WWII Colt production"
+    notes: "WWII Colt production"
+},
+
+/* SPRINGFIELD ARMORY */
+
+{
+    manufacturer: "Springfield Armory",
+    year: "1914-1915",
+    serial_start: 72571,
+    serial_end: 83855,
+    notes: "Springfield Armory production. 1915 production range: 81751 - 83855."
 },
 
 /* ITHACA */
@@ -171,8 +91,9 @@ var pistols = [
     year: "1943",
     serial_start: 856405,
     serial_end: 916404,
-    notes: "Duplicate Ithaca serial range. Verify authenticity with F.J.A. inspection mark"
+    notes: "Duplicate Ithaca serial range. Verify authenticity with F.J.A. inspection mark."
 },
+
 {
     manufacturer: "Ithaca",
     year: "1943",
@@ -180,6 +101,7 @@ var pistols = [
     serial_end: 1279673,
     notes: "Ithaca WWII production"
 },
+
 {
     manufacturer: "Ithaca",
     year: "1943",
@@ -187,6 +109,7 @@ var pistols = [
     serial_end: 1471430,
     notes: "Ithaca WWII production"
 },
+
 {
     manufacturer: "Ithaca",
     year: "1944",
@@ -194,6 +117,7 @@ var pistols = [
     serial_end: 1890503,
     notes: "Ithaca WWII production"
 },
+
 {
     manufacturer: "Ithaca",
     year: "1945",
@@ -201,6 +125,7 @@ var pistols = [
     serial_end: 2134403,
     notes: "Late Ithaca WWII production"
 },
+
 {
     manufacturer: "Ithaca",
     year: "1945",
@@ -218,6 +143,7 @@ var pistols = [
     serial_end: 1041404,
     notes: "Early Remington Rand WWII production"
 },
+
 {
     manufacturer: "Remington Rand",
     year: "1943",
@@ -225,6 +151,7 @@ var pistols = [
     serial_end: 1441430,
     notes: "Remington Rand WWII production"
 },
+
 {
     manufacturer: "Remington Rand",
     year: "1943",
@@ -232,6 +159,7 @@ var pistols = [
     serial_end: 1609528,
     notes: "Remington Rand WWII production"
 },
+
 {
     manufacturer: "Remington Rand",
     year: "1944",
@@ -239,6 +167,7 @@ var pistols = [
     serial_end: 2075103,
     notes: "Remington Rand WWII production"
 },
+
 {
     manufacturer: "Remington Rand",
     year: "1945",
@@ -246,32 +175,13 @@ var pistols = [
     serial_end: 2244803,
     notes: "Late Remington Rand WWII production"
 },
+
 {
     manufacturer: "Remington Rand",
     year: "1945",
     serial_start: 2380014,
     serial_end: 2619013,
     notes: "Final Remington Rand WWII production"
-},
-
-/* UNION SWITCH & SIGNAL */
-
-{
-    manufacturer: "Union Switch & Signal",
-    year: "1943",
-    serial_start: 1041405,
-    serial_end: 1096404,
-    notes: "US&S WWII production"
-},
-
-/* SINGER */
-
-{
-    manufacturer: "Singer",
-    year: "1942",
-    serial_start: "S800001",
-    serial_end: "S800500",
-    notes: "Singer contract pistols"
 }
 
 ];
