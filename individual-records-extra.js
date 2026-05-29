1b26054bd24a8a9f87426703cc1ab61955d4b9e8
PDFuse <davidikuta@gmail.com>
2026-05-29 14:19:40 -0500

    Add Colt Navy 538 individual record mapping

--- individual-records-extra.js
+++ individual-records-extra.js
@@ -1,4 +1,5 @@
 if (typeof individualRecordPages !== 'undefined') {
     individualRecordPages['42'] = 'pistols/42.html';
+    individualRecordPages['538'] = 'pistols/538.html';
     individualRecordPages['1828605'] = 'pistols/1828605.html';
 }
