e1dfc41d52e831ab9492e79eb9e3a850d3b721af
PDFuse <davidikuta@gmail.com>
2026-05-29 14:57:00 -0500

    Add Ithaca 1450575 FOIA record mapping

--- individual-records-extra.js
+++ individual-records-extra.js
@@ -1,5 +1,6 @@
 if (typeof individualRecordPages !== 'undefined') {
     individualRecordPages['42'] = 'pistols/42.html';
     individualRecordPages['538'] = 'pistols/538.html';
+    individualRecordPages['1450575'] = 'pistols/1450575.html';
     individualRecordPages['1828605'] = 'pistols/1828605.html';
 }
