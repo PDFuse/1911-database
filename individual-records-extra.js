a1914132734c2b04499630d71e4b39c46b7a0790
PDFuse <davidikuta@gmail.com>
2026-05-29 17:06:59 -0500

    Add Ithaca 1854428 individual record mapping

--- individual-records-extra.js
+++ individual-records-extra.js
@@ -3,4 +3,5 @@ if (typeof individualRecordPages !== 'undefined') {
     individualRecordPages['538'] = 'pistols/538.html';
     individualRecordPages['1450575'] = 'pistols/1450575.html';
     individualRecordPages['1828605'] = 'pistols/1828605.html';
+    individualRecordPages['1854428'] = 'pistols/1854428.html';
 }
