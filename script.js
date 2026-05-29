de94cf67befcbbb558ed6ca33b82bf7c84dda39f
PDFuse <davidikuta@gmail.com>
2026-05-29 09:45:57 -0500

    Add searchable Colt 707356 record

--- script.js
+++ script.js
@@ -21,6 +21,7 @@ var individualRecordPages = {
     "369379": "pistols/369379.html",
     "388733": "pistols/388733.html",
     "702585": "pistols/702585.html",
+    "707356": "pistols/707356.html",
     "817679": "pistols/817679.html",
     "861420": "pistols/861420.html",
     "1068267": "pistols/1068267.html",
@@ -102,11 +103,11 @@ function getIndividualPageLink(input) {
     var serial = normalizeSerial(input);
 
     if (individualRecordPages[serial]) {
-        return "<p><a href='" + escapeHtml(individualRecordPages[serial]) + "?v=20260529c'>Open Individual Record →</a></p>";
+        return "<p><a href='" + escapeHtml(individualRecordPages[serial]) + "?v=20260529d'>Open Individual Record →</a></p>";
     }
 
     if (typeof pistolRecords !== "undefined" && pistolRecords[serial]) {
-        return "<p><a href='pistols/" + encodeURIComponent(serial) + ".html?v=20260529c'>Open Individual Record →</a></p>";
+        return "<p><a href='pistols/" + encodeURIComponent(serial) + ".html?v=20260529d'>Open Individual Record →</a></p>";
     }
 
     return "";
