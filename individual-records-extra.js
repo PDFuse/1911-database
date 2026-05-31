ea983e1ebe54ee60c64185c31fd22914c9fe0290
PDFuse <davidikuta@gmail.com>
2026-05-31 16:35:18 -0500

    Add CMP observation records to individual page map

--- individual-records-extra.js
+++ individual-records-extra.js
@@ -4,4 +4,24 @@ if (typeof individualRecordPages !== 'undefined') {
     individualRecordPages['1450575'] = 'pistols/1450575.html';
     individualRecordPages['1828605'] = 'pistols/1828605.html';
     individualRecordPages['1854428'] = 'pistols/1854428.html';
+
+    individualRecordPages['1926885'] = 'pistols/1926885.html';
+    individualRecordPages['1887296'] = 'pistols/1887296.html';
+    individualRecordPages['1311841'] = 'pistols/1311841.html';
+    individualRecordPages['730058'] = 'pistols/730058.html';
+    individualRecordPages['1754060'] = 'pistols/1754060.html';
+    individualRecordPages['1166186'] = 'pistols/1166186.html';
+    individualRecordPages['2108757'] = 'pistols/2108757.html';
+    individualRecordPages['1007642'] = 'pistols/1007642.html';
+    individualRecordPages['1833662'] = 'pistols/1833662.html';
+    individualRecordPages['2267082'] = 'pistols/2267082.html';
+    individualRecordPages['1136852'] = 'pistols/1136852.html';
+    individualRecordPages['714093'] = 'pistols/714093.html';
+    individualRecordPages['1825392'] = 'pistols/1825392.html';
+    individualRecordPages['1153716'] = 'pistols/1153716.html';
+    individualRecordPages['1129767'] = 'pistols/1129767.html';
+    individualRecordPages['1383505'] = 'pistols/1383505.html';
+    individualRecordPages['1589098'] = 'pistols/1589098.html';
+    individualRecordPages['1272220'] = 'pistols/1272220.html';
+    individualRecordPages['1513922'] = 'pistols/1513922.html';
 }
